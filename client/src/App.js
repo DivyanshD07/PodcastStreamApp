f7496a65dd28c9e7a19b34a0765040c5900a5b7d
Divyansh Dhyani <devdhyani34@gmail.com>
2024-07-08 16:36:03 +0530

    Basic setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <div className="App">
+      Podstream
+    </div>
+  );
+}
+
+export default App;
